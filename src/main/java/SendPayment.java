de2f8ae5638cada397e74d2b1fcaa87d52b6e5bb
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-02-05 23:56:30 +0600

    Create class SendPayment

---
+++ src/main/java/SendPayment.java
@@ -0,0 +1,25 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SendPayment {
+    private UUID paymentId;
+    private UUID payeeId;
+    private String payoutMemo;
+    private Long amount;
+    private String payeeFirstName;
+    private String payeeLastName;
+    private String payeeAddressLine1;
+    private String payeeAddressCity;
+    private String payeeAddressStateOrProv;
+    private String payeeAddressZipOrPostal;
+    private String payeeAddressCountryCode;
+    private String routingNumber;
+    private String accountNumber;
+    private String accountName;
+}
